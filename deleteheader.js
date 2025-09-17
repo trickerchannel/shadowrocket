f4048529f94e28af77e6ecdf04aa108928d9c69d
Tricker Channel <119558238+trickerchannel@users.noreply.github.com>
2025-09-17 10:41:06 +0700

    Update deleteheader.js

--- deleteheader.js
+++ deleteheader.js
@@ -1,2 +1,20 @@
-const version = 'V1.0.2';
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
+// Updated deleteHeader.js
+// ========= Header Modification ========= //
+const version = 'V1.0.3';
+
+function setHeaderValue(e, a, d) {
+  var r = a.toLowerCase();
+  r in e ? e[r] = d : e[a] = d;
+}
+
+// Lấy headers hiện tại từ request
+var modifiedHeaders = $request.headers;
+
+// Thay đổi giá trị của X-RevenueCat-ETag
+setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+
+// Debug: In header đã sửa (tuỳ chọn)
+console.log("Modified Headers:", JSON.stringify(modifiedHeaders));
+
+// Kết thúc request với header đã sửa đổi
+$done({ headers: modifiedHeaders });
